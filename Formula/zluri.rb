83b19daa3b1b6085bd70fadd0ad71a549cdbc063
srinivasas-zluri <srinivasa.s@zluri.com>
2025-10-03 13:01:08 +0530

    feat(scripts): +temp scripts

---
+++ Formula/zluri.rb
@@ -0,0 +1,15 @@
+class Zluri < Formula
+  desc "Zluri CLI tool"
+  homepage "https://github.com/srinivasas-zluri/zluri-scripts"
+  url "https://github.com/srinivasas-zluri/zluri-scripts/releases/download/v1.0.0/zluri-macos-v1.0.0.tar.gz"
+  sha256 "a6b2f92539ea689cef594ddb34f46e3d9027cacc849b9661b74ebf43c3ff2265"
+  license "MIT"
+
+  depends_on "gh"
+  depends_on "skim"
+
+  def install
+    bin.install "zluri"
+    bin.install "zluri-deploy"
+  end
+end
