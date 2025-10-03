0e3119f1be41765eeba5938530dbc36e6724f909
srinivasas-zluri <srinivasa.s@zluri.com>
2025-10-03 13:23:32 +0530

    test(feat-bump): feat bump for testing

--- Formula/zluri.rb
+++ Formula/zluri.rb
@@ -1,8 +1,8 @@
 class Zluri < Formula
   desc "Zluri CLI tool"
   homepage "https://github.com/srinivasas-zluri/zluri-scripts"
-  url "https://github.com/srinivasas-zluri/zluri-scripts/releases/download/v1.0.0/zluri-macos-v1.0.0.tar.gz"
-  sha256 "a6b2f92539ea689cef594ddb34f46e3d9027cacc849b9661b74ebf43c3ff2265"
+  url "https://github.com/srinivasas-zluri/zluri-scripts/releases/download/v1.0.1/zluri-macos-v1.0.1.tar.gz"
+  sha256 "2a9be295e13f4bb49995eedc0a055e1ca2e3c1e8aaac2bc874b69da474a5b703"
   license "MIT"
 
   depends_on "gh"
